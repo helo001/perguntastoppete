71fbc72661e82c53bb519285bb0b9cb87d7e07d7
helo001 <142610590+helo001@users.noreply.github.com>
2024-09-27 11:02:11 -0300

    Add files via upload

---
+++ 13aulas-main/13aulas-main/perguntas.js
@@ -0,0 +1,73 @@
+criaCartao(
+    'Astronomia',
+    'Qual é a constelação mais fácil e identificar no céu noturno?',
+    ' Órion,mas o que muitos não sabem é que a estrela trê maria fazem parte dela.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que significa o nome biologia?',
+    'Significa "estudo da vida" (bio=vida logos=etudo)'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a região do Paraná?',
+    'Região Sul'
+)
+
+criaCartao(
+    'Inglês',
+    'Como se diz sol em inglês?',
+    'Sun'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a raiz quadrada de 81?',
+    '9!'
+)
+
+criaCartao(
+    'Química',
+    'O que significa a fórmula H2O?',
+    'É a fórmula da água'
+)
+
+criaCartao(
+    'Física',
+    'Por qual descoberta Albert Einstein ganhou o prêmio nobel de física?',
+    'Efeito fotoelétrico'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é uma célula?',
+    'É uma unidade básica da vida'
+)
+
+
+criaCartao(
+    'Física',
+    'O que é força?',
+    'É uma interação que pode causar a mudança no movimento de um objeto'
+)
+
+
+criaCartao(
+    'Esporte',
+    'Qual o objetivo do futebol?',
+    'É marcar mais gols que o time adversário'
+)
+
+criaCartao(
+    'Artes',
+'Quem fez a famosa pintura "A Noite estrelada"?',
+    'Vincent van Gogh'
+)
+
+criaCartao(
+    'Ciêcias',
+    'Quantos planetas tem no sistema solar?',
+    '8!'
+)
